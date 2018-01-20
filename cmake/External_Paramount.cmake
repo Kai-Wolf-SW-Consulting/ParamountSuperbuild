772389818f3f3103346d6d93c33148a07dffaef6
Kai Wolf <kai.wolf@gmail.com>
2018-01-20 15:14:04 +0100

    Add dependencies for building Paramount project

--- cmake/External_Paramount.cmake
+++ cmake/External_Paramount.cmake
@@ -1,7 +1,19 @@
+if(NOT USE_SYSTEM_BOOST)
+  list(APPEND deps boost)
+endif()
+
+if(NOT USE_SYSTEM_QT)
+  list(APPEND deps qt5)
+endif()
+
+if(NOT USE_SYSTEM_GTEST)
+  list(APPEND deps gtest)
+endif()
+
 ExternalProject_Add(Paramount
   SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Paramount
   BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/Paramount
   CMAKE_CACHE_ARGS
     ${ParamountSuperbuild_DEFAULT_ARGS}
-    ${ParamountSuperbuild_THIRDPARTYLIBS_ARGS})
-
+    ${ParamountSuperbuild_THIRDPARTYLIBS_ARGS}
+  DEPENDS ${deps})
