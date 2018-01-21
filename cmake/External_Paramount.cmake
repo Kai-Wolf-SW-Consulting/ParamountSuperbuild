5dee4eb33dfe0196bc613b0512ae8c5f28a8d619
Kai Wolf <kai.wolf@gmail.com>
2018-01-21 19:41:51 +0100

    Add more cleaner solution for integrating Paramount into superbuild

--- cmake/External_Paramount.cmake
+++ cmake/External_Paramount.cmake
@@ -12,15 +12,13 @@ endif()
 
 ExternalProject_Add(Paramount
   SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Paramount
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -E echo
-    "Generating cache script for Paramount..."
-  BUILD_COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_BINARY_DIR}
-    ${CMAKE_COMMAND} ${CMAKE_CURRENT_SOURCE_DIR}
-  INSTALL_COMMAND ""
+  CMAKE_CACHE_ARGS
+    ${ParamountSuperbuild_DEFAULT_ARGS}
+    ${ParamountSuperbuild_THIRDPARTYLIBS_ARGS}
   DEPENDS ${deps})
 
-set(cache_script ${CMAKE_CURRENT_BINARY_DIR}/ParamountCache.cmake)
+set(paramount_cache_script ${CMAKE_CURRENT_BINARY_DIR}/ParamountCache.cmake
+  CACHE FILEPATH "Path to initial cache file for configuring Paramount")
 set(cache_args ${ParamountSuperbuild_DEFAULT_ARGS}
     ${ParamountSuperbuild_THIRDPARTYLIBS_ARGS})
-write_cmake_cache_file("${cache_script}" "${cache_args}")
-set(Paramount_deps_installed TRUE CACHE BOOL "")
+write_cmake_cache_file("${paramount_cache_script}" "${cache_args}")
