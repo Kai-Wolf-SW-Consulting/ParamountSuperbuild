aa9e79dafbbe12cf5949670c463a3d8d6fe7d598
Kai Wolf <kai.wolf@gmail.com>
2018-01-25 06:58:40 +0100

    Small fix

--- cmake/External_Paramount.cmake
+++ cmake/External_Paramount.cmake
@@ -14,6 +14,8 @@ ExternalProject_Add(Paramount
   SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Paramount
   DEPENDS ${deps})
 
+# CMake automatically generates a init cache script file for us, which we can
+# use to configure this subproject via add_subdirectory()
 ExternalProject_Get_property(Paramount TMP_DIR)
 set(paramount_cache_script ${TMP_DIR}/Paramount-cache-${CMAKE_BUILD_TYPE}.cmake
   CACHE FILEPATH "Path to initial cache file for configuring Paramount")
